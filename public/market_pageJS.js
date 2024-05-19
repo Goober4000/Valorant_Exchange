71b716dd0e91dec24138b091bd6a29a1004f0bef
GabrielDuran1 <gabrieleduran360@gmail.com>
2024-05-19 13:12:19 -0400

    Fixing table for market

--- public/market_pageJS.js
+++ public/market_pageJS.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('postings')  
+    fetch('postings') 
         .then(response => response.json())
         .then(data => {
             const tableBody = document.getElementById('customersTable').getElementsByTagName('tbody')[0];
             data.forEach(customer => {
                 const row = tableBody.insertRow();
+                const idCell = row.insertCell(0);
                 const nameCell = row.insertCell(1);
                 const contextCell = row.insertCell(2);
 
+                idCell.textContent = customer.id;
                 nameCell.textContent = customer.name;
                 contextCell.textContent = customer.context;
             });
